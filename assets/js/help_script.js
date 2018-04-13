212781601751acd668731190b500db859b98edd4
Amanda <amanda-heij@hotmail.com>
2018-04-13 14:22:45 +0200

    tilføjelse af java til FAQ boks m.m
    tilføjet link til de andre sider så det linker til help.html
    
    tilføjet flag til assets i png
    
    ændringer i vejledningsboksne

---
+++ assets/js/help_script.js
@@ -0,0 +1,62 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function myFunction1() {
+    var x = document.getElementById("myDIV1");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("myDIV2");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function myFunction3() {
+    var x = document.getElementById("myDIV3");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function myFunction4() {
+    var x = document.getElementById("myDIV4");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function myFunction5() {
+    var x = document.getElementById("myDIV5");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function myFunction6() {
+    var x = document.getElementById("myDIV6");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
