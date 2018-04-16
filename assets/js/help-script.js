5f60a90ad29a837c0d853713cc64f18e50def153
Mathias Poulsen <mathiiaspoulsen@gmail.com>
2018-04-16 11:20:39 +0200

    minus sign on help + fonts are imported one at a time on every page

--- assets/js/help-script.js
+++ assets/js/help-script.js
@@ -1,62 +1,90 @@
 function myFunction() {
     var x = document.getElementById("myDIV");
+    var btn = document.querySelector("#btn0");
+
     if (x.style.display == "block") {
         x.style.display = "none";
+        btn.innerText = "+";
     } else {
         x.style.display = "block";
+        btn.innerText = String.fromCharCode(0x2212);
     }
 }
 
 function myFunction1() {
     var x = document.getElementById("myDIV1");
+    var btn = document.querySelector("#btn1");
+
     if (x.style.display == "block") {
         x.style.display = "none";
+        btn.innerText = "+";
     } else {
         x.style.display = "block";
+        btn.innerText = String.fromCharCode(0x2212);
     }
 }
 
 function myFunction2() {
     var x = document.getElementById("myDIV2");
+    var btn = document.querySelector("#btn2");
+
     if (x.style.display == "block") {
         x.style.display = "none";
+        btn.innerText = "+";
     } else {
         x.style.display = "block";
+        btn.innerText = String.fromCharCode(0x2212);
     }
 }
 
 function myFunction3() {
     var x = document.getElementById("myDIV3");
+    var btn = document.querySelector("#btn3");
+
     if (x.style.display == "block") {
         x.style.display = "none";
+        btn.innerText = "+";
     } else {
         x.style.display = "block";
+        btn.innerText = String.fromCharCode(0x2212);
     }
 }
 
 function myFunction4() {
     var x = document.getElementById("myDIV4");
+    var btn = document.querySelector("#btn4");
+
     if (x.style.display == "block") {
         x.style.display = "none";
+        btn.innerText = "+";
     } else {
         x.style.display = "block";
+        btn.innerText = String.fromCharCode(0x2212);
     }
 }
 
 function myFunction5() {
     var x = document.getElementById("myDIV5");
+    var btn = document.querySelector("#btn5");
+
     if (x.style.display == "block") {
         x.style.display = "none";
+        btn.innerText = "+";
     } else {
         x.style.display = "block";
+        btn.innerText = String.fromCharCode(0x2212);
     }
 }
 
 function myFunction6() {
     var x = document.getElementById("myDIV6");
+    var btn = document.querySelector("#btn6");
+
     if (x.style.display == "block") {
         x.style.display = "none";
+        btn.innerText = "+";
     } else {
         x.style.display = "block";
+        btn.innerText = String.fromCharCode(0x2212);
     }
 }
