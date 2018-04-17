169a22e841acd1fc89c73b9cb825bfaf581d782f
Mathias Poulsen <mathiiaspoulsen@gmail.com>
2018-04-17 10:26:12 +0200

    respnsiveness

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -2,9 +2,20 @@ window.addEventListener("resize", function() {
     let width = document.body.clientWidth;
 
     if (width < 1280) {
-      if (document.querySelector("#velkommen-billede")) {        
+      if (document.querySelector("#velkommen-billede")) {
         let image = document.querySelector("#velkommen-billede");
         image.parentNode.removeChild(image);
       }
     }
 });
+
+window.onload = function() {
+  let width = document.body.clientWidth;
+
+  if (width < 1280) {
+    if (document.querySelector("#velkommen-billede")) {
+      let image = document.querySelector("#velkommen-billede");
+      image.parentNode.removeChild(image);
+    }
+  }
+}
